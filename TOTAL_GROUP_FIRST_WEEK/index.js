d975d820fd8d405032bc00a91101930b8dd7af78
BossNumber1 <dimaang23@gmail.com>
2022-01-06 13:07:19 +0300

    up

--- TOTAL_GROUP_FIRST_WEEK/index.js
+++ TOTAL_GROUP_FIRST_WEEK/index.js
@@ -1566,3 +1566,67 @@ document.getElementById("submit").onclick = function () {
     question19();
     question20();
 };
+
+// -------------------------------------------------------------- ROTATE RULER FOR 14 QUESTION -----------------------------------------------
+
+let ruler14question = document.getElementById("ruler14");
+
+dragElement(ruler14question);
+
+const rotationFunction = new Propeller(ruler14question, {
+    inertia: 0,
+});
+
+function dragElement(element) {
+    let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = element.offsetTop - pos2 + "px";
+        element.style.left = element.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+ruler14question.addEventListener("mousedown", (e) => {
+    if (
+        (e.target.className !== "leftEdge" ||
+            e.target.className !== "rightEdge") &&
+        e.target.className === "imgRuler"
+    ) {
+        rotationFunction.stop();
+        dragElement(ruler14question);
+    }
+
+    if (
+        (e.target.className === "leftEdge" ||
+            e.target.className === "rightEdge") &&
+        e.target.className !== "imgRuler"
+    ) {
+        rotationFunction.onRotated(e);
+    }
+});
